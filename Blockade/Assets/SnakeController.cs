750819a2af5bacb542978b24e878ba51b4552f5a
Ashwin <aktanuku96@gmail.com>
2018-10-21 21:42:21 -0400

    fixed the movement

--- Blockade/Assets/SnakeController.cs
+++ Blockade/Assets/SnakeController.cs
@@ -8,21 +8,22 @@ public class SnakeController : MonoBehaviour {
 
     // Use this for initialization
     Vector2 direction = Vector2.down;
-    bool movePOne = false;
-    bool movePTwo = false;
-
+    Vector2 pos;
+    public float speed;
     public GameObject PlayerOnePreFab;
-    public GameObject PlayerTwoPreFab;
+    public int count;
 
     List<Transform> tailOne = new List<Transform>();
 
 	void Start () {
-        InvokeRepeating("Move", 0.02f, 0.02f);
+        //InvokeRepeating("Move", 0.02f, 0.02f);
+        //Vector2 pos = transform.position;
+    
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        
+        count += 1;
 
         if (Input.GetKey(KeyCode.S) && gameObject.tag.Equals("PlayerOne"))
             direction = Vector2.down;
@@ -41,7 +42,13 @@ public class SnakeController : MonoBehaviour {
         else if (Input.GetKey(KeyCode.RightArrow) && gameObject.tag.Equals("PlayerTwo"))
             direction = Vector2.right;
 
-
+        if (count % 100 == 0)
+        {
+            Move();
+            Vector2 v = transform.position;
+            GameObject g = (GameObject)Instantiate(PlayerOnePreFab, v, Quaternion.identity);
+            tailOne.Insert(0, g.transform);
+        }
         //if(gameObject.tag.Equals("PlayerOne")){
         //    Vector2 v = transform.position;
         //     GameObject g = (GameObject)Instantiate(PlayerOnePreFab, v, Quaternion.identity);
@@ -55,23 +62,25 @@ public class SnakeController : MonoBehaviour {
         if(Input.GetKey(KeyCode.R)){
             UnityEngine.SceneManagement.SceneManager.LoadScene("LevelOne");
         }
+
+
     }
 
     void Move()
     {
-        Vector2 v = transform.position;
-       // GameObject g = (GameObject)Instantiate(PlayerOnePreFab, v, Quaternion.identity);
-       // tailOne.Insert(0, g.transform);
-        transform.Translate(direction * Time.deltaTime *2) ;
+
+        transform.Translate(direction * speed);
         //if(tailOne.Count > 0){
         //    tailOne.Last().position = v;
         //    tailOne.Insert(0, tailOne.Last());
         //}
+        
+      
     }
 
     void OnCollisionEnter(Collision collision)
     {
-        //CancelInvoke();
+
         Debug.Log("Hi");
         //if(collision.gameObject.tag.Equals("Wall"))
         //CancelInvoke();
