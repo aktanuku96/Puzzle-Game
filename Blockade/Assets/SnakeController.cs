959800ca765cd4ab4ec36266ee4bcce0be2a341c
Ashwin <aktanuku96@gmail.com>
2018-10-22 02:19:43 -0400

    Collisions still iffy, and the scores are in, need to figure out how to maintain scores while reseting

--- Blockade/Assets/SnakeController.cs
+++ Blockade/Assets/SnakeController.cs
@@ -2,23 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class SnakeController : MonoBehaviour {
 
     // Use this for initialization
     Vector2 direction = Vector2.down;
-    Vector2 pos;
+
     public float speed;
     public GameObject PlayerOnePreFab;
     public int count;
+    public Text PlayerOneScore;
+    public Text PlayerTwoScore;
+
+    private int triggerCount;
+    private bool isHit;
+
+    private int pauseCount;
+    private int playerOneScoreCount;
+    private int playerTwoScoreCount;
+
 
     List<Transform> tailOne = new List<Transform>();
 
 	void Start () {
         //InvokeRepeating("Move", 0.02f, 0.02f);
         //Vector2 pos = transform.position;
-    
+        //PlayerOneScore= GetComponent<Text>();
+        //PlayerTwoScore= GetComponent<Text>();
+        playerOneScoreCount = 0;
+        playerTwoScoreCount = 0;
+       // pauseCount = 0;
+        //StartCoroutine(BlinkText());
 	}
 	
 	// Update is called once per frame
@@ -42,62 +58,86 @@ public class SnakeController : MonoBehaviour {
         else if (Input.GetKey(KeyCode.RightArrow) && gameObject.tag.Equals("PlayerTwo"))
             direction = Vector2.right;
 
-        if (count % 100 == 0)
+        if (count % 50 == 0)
         {
             Move();
-            Vector2 v = transform.position;
-            GameObject g = (GameObject)Instantiate(PlayerOnePreFab, v, Quaternion.identity);
-            tailOne.Insert(0, g.transform);
+           
         }
-        //if(gameObject.tag.Equals("PlayerOne")){
-        //    Vector2 v = transform.position;
-        //     GameObject g = (GameObject)Instantiate(PlayerOnePreFab, v, Quaternion.identity);
-        //     tailOne.Insert(0, g.transform);
-        //    if(tailOne.Count > 0){
-        //        tailOne.Last().position = v;
-        //        tailOne.Insert(0, tailOne.Last());
-        //    }
-        //}
 
-        if(Input.GetKey(KeyCode.R)){
+
+        if (Input.GetKey(KeyCode.R))
+        {
             UnityEngine.SceneManagement.SceneManager.LoadScene("LevelOne");
         }
-
-
     }
 
     void Move()
     {
-
+        Vector2 v = transform.position;
         transform.Translate(direction * speed);
+       // GameObject g = (GameObject)Instantiate(PlayerOnePreFab, v, Quaternion.identity);
+        //tailOne.Insert(0, g.transform);
+
         //if(tailOne.Count > 0){
         //    tailOne.Last().position = v;
+
         //    tailOne.Insert(0, tailOne.Last());
+        //    tailOne.RemoveAt(tailOne.Count - 1);
         //}
-        
       
     }
 
-    void OnCollisionEnter(Collision collision)
+    void OnTriggerEnter2D(Collider2D other)
     {
-
-        Debug.Log("Hi");
-        //if(collision.gameObject.tag.Equals("Wall"))
-        //CancelInvoke();
-        if (collision.gameObject.tag.Equals("Wall"))
+        if (other.gameObject.tag.Equals("Wall") && gameObject.tag.Equals("PlayerTwo")){
+            Debug.Log("HITTTTT THE WALLL");
+            transform.Translate(direction*speed);
+            speed = 0;
+            playerOneScoreCount++;
+            SetPointsText();
+            //UnityEngine.SceneManagement.SceneManager.LoadScene("LevelOne");
+        }
+        if (other.gameObject.tag.Equals("Wall") && gameObject.tag.Equals("PlayerOne"))
         {
             Debug.Log("HITTTTT THE WALLL");
-            CancelInvoke();
+            transform.Translate(direction * speed);
+            speed = 0;
+            playerTwoScoreCount++;
+            SetPointsText();
         }
-
-    }
-
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.tag.Equals("Wall")){
+        if (other.gameObject.tag.Equals("PlayerTwo") && gameObject.tag.Equals("PlayerOne"))
+        {
             Debug.Log("HITTTTT THE WALLL");
-            CancelInvoke();
+            transform.Translate(direction * speed);
+            speed = 0;
+            playerOneScoreCount++;
+            SetPointsText();
         }
+        if (other.gameObject.tag.Equals("PlayerOne") && gameObject.tag.Equals("PlayerOne"))
+        {
+            Debug.Log("HITTTTT THE WALLL");
+            transform.Translate(direction * speed);
+            speed = 0;
+            playerTwoScoreCount++;
+            SetPointsText();
+        }
+
+
     }
 
+    void SetPointsText()
+    {
+        PlayerOneScore.text = playerOneScoreCount.ToString();
+        PlayerTwoScore.text = playerTwoScoreCount.ToString();
+      
+    }
+
+    //public IEnumerator BlinkText(){
+    //    while(true){
+    //        PlayerOneScore.text = "" + playerOneScoreCount;
+    //        yield return new WaitForSeconds(.5f);
+    //        PlayerTwoScore.text = "" + playerTwoScoreCount;
+    //        yield return new WaitForSeconds(.5f);
+    //    }
+    //}
 }
