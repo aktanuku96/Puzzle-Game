329a3c7a0eede3e65ba742681daf3738e372ad29
Ashwin <aktanuku96@gmail.com>
2018-11-05 03:07:59 -0500

    Final Clone was made to the best possible version

--- Blockade/Assets/Scripts/GamePlay.cs
+++ Blockade/Assets/Scripts/GamePlay.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GamePlay : MonoBehaviour {
 
@@ -9,25 +11,140 @@ public class GamePlay : MonoBehaviour {
     public GameObject[] players;
     public SnakeController[] playerScripts = new SnakeController[2];
 
-	// Use this for initialization
-	void Start () {
+    //public Text PlayerOneScore;
+    //public Text PlayerTwoScore;
+
+    public Text[] Scores;
+    public Text GameOverText;
+
+    //public int playerOneScoreCount;
+    //public int playerTwoScoreCount;
+    public int[] ScoreCounts = new int[2];
+
+
+    private bool stall;
+    // Use this for initialization
+    void Start () {
+        GameOverText.enabled = false;
+        //playerOneScoreCount = 0;
+        //playerTwoScoreCount = 0;
 
         playerScripts[0] = players[0].GetComponent<SnakeController>();
         playerScripts[1] = players[1].GetComponent<SnakeController>();
 
+
+        Debug.Log(playerPositions[0].position);
+        Debug.Log(playerPositions[1].position);
         players[0].transform.position = playerPositions[0].position;
         players[1].transform.position = playerPositions[1].position;
-	}
+
+        Scores[0].text = "Player One: " + ScoreCounts[0].ToString();
+        Scores[1].text = "Player Two: " + ScoreCounts[1].ToString();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        if(playerScripts[0].isHit || playerScripts[1].isHit){
-            playerScripts[0].enabled = false;
-            playerScripts[1].enabled = false;
-        }
+        Debug.Log(playerPositions[0].position);
+        Debug.Log(playerPositions[1].position);
+        Debug.Log("here is the transform 0: " + players[0].transform.position);
+        Debug.Log("here is the transform 1: " + players[1].transform.position);
+
+        Scores[0].text = "Player One: " + ScoreCounts[0].ToString();
+        Scores[1].text = "Player Two: " + ScoreCounts[1].ToString();
+
         if (Input.GetKey(KeyCode.R))
         {
             UnityEngine.SceneManagement.SceneManager.LoadScene("LevelOne");
         }
+
+        //ScoreCounts[0].enabled = false;
+        //ScoreCounts[1].enabled = false;
+        if (stall) return;
+        if (playerScripts[0].isHit || playerScripts[1].isHit)
+        {
+            if(playerScripts[0].isHit && playerScripts[1].isHit){
+                //ScoreCounts[0].enabled = true;
+                //ScoreCounts[1].enabled = true;
+                ScoreCounts[0]++;
+                ScoreCounts[1]++;
+                //ScoreCounts[0].enabled = false;
+                //ScoreCounts[1].enabled = false;
+                //PlayerOneScore.text = "Player One: " + playerOneScoreCount.ToString();
+                //PlayerTwoScore.text = "Player Two: " + playerTwoScoreCount.ToString();
+            }
+
+            else if(playerScripts[0].isHit){
+                //ScoreCounts[1].enabled = true;
+                ScoreCounts[1]++;
+                //ScoreCounts[1].enabled = false;
+                // PlayerTwoScore.text = "Player Two: " + playerTwoScoreCount.ToString();
+            }
+
+            else if(playerScripts[1].isHit){
+                //ScoreCounts[0].enabled = true;
+                ScoreCounts[0]++;
+                //ScoreCounts[0].enabled = false;
+                //PlayerOneScore.text = "Player One: " + playerOneScoreCount.ToString();
+            }
+
+            //Scores[0].enabled = true;
+            //Scores[1].enabled = true;
+            StartCoroutine(ResetRound());
+        }
     }
+
+    IEnumerator ResetRound(){
+        Debug.Log("it started");
+        stall = true;
+        GameObject[] tailObjects = GameObject.FindGameObjectsWithTag("Tail");
+        //Destroy(tailObjects[0]);
+        //Destroy(tailObjects[1]);
+
+        playerScripts[0].enabled = false;
+        playerScripts[1].enabled = false;
+
+        yield return new WaitForSeconds(2.5f);
+
+        Debug.Log("The wait has ended");
+        tailObjects = GameObject.FindGameObjectsWithTag("Tail");
+        Debug.Log(tailObjects.Length);
+        foreach (var tailObject in tailObjects)
+        {
+            //Debug.Log("destroying");
+            Destroy(tailObject);
+           //Debug.Log("destroying");
+        }
+
+        //Debug.Log(playerPositions[0].position);
+        //Debug.Log(playerPositions[1].position);
+        playerPositions[0].position = new Vector3(2.5f, 16.5f, 1.0f);
+        playerPositions[1].position = new Vector3(25.5f, 0.5f, 1.0f);
+        players[0].transform.position = playerPositions[0].position;
+        players[1].transform.position = playerPositions[1].position;
+
+        if (ScoreCounts[0] >= scoreLimit || ScoreCounts[1] >= scoreLimit) {
+            GameOverText.enabled = true;
+        }
+
+        else{
+
+            playerScripts[0].direction = Vector2.down;
+            playerScripts[1].direction = Vector2.up;
+            //players[0].transform.rotation = playerScripts[0].startPosition;
+            //players[1].transform.rotation = playerScripts[1].startPosition;
+
+            playerScripts[0].enabled = true;
+            playerScripts[1].enabled = true;
+
+
+
+            playerScripts[0].isHit = false;
+            playerScripts[1].isHit = false;
+
+            stall = false;
+        }
+
+
+    }
+
 }
--- Blockade/Assets/Scripts/SnakeController.cs
+++ Blockade/Assets/Scripts/SnakeController.cs
@@ -8,37 +8,26 @@ using UnityEngine.SceneManagement;
 public class SnakeController : MonoBehaviour {
 
     // Use this for initialization
-    Vector2 direction;
+    public Vector2 direction;
 
-    //public float speedOne;
-    //public float speedTwo;
-    public float speed;
+   //public float speed;
 
     public GameObject PlayerOnePreFab;
     public int count;
-    public Text PlayerOneScore;
-    public Text PlayerTwoScore;
+
     public bool isHit;
 
     public AudioClip sound;
     public AudioSource aud;
 
     private Vector2 pos;
-    private int playerOneScoreCount;
-    private int playerTwoScoreCount;
-
-
-
+   
 	void Start () {
-
-        playerOneScoreCount = 0;
-        playerTwoScoreCount = 0;
 
         aud.clip = sound;
 
-
         isHit = false;
-        //Debug.Log("Is it Hit? : " + isHit + " " + gameObject.name);
+
         if(gameObject.tag.Equals("PlayerOne")){
             direction = Vector2.down;
 
@@ -78,176 +67,31 @@ public class SnakeController : MonoBehaviour {
             if(isHit == false){
 
                 Move();
-                //Instantiate(PlayerOnePreFab, pos, transform.rotation);
             }
 
-            else{
-                Debug.Log("hi");
-                //Stop();
-            }
-
-            //else{
-                //if(gameObject.tag.Equals("PlayerOne") || gameObject.tag.Equals("PlayerTwo"))
-                //{
-                //    speed = 0;
-                //}
-               
-                //Debug.Log("all should stop");
-            //}
-           
         }
 
-
-        if (Input.GetKey(KeyCode.R))
-        {
-            UnityEngine.SceneManagement.SceneManager.LoadScene("LevelOne");
-        }
     }
 
     void Move()
     {
         aud.PlayOneShot(sound);
 
-    
         transform.position += (Vector3)(direction);
         Instantiate(PlayerOnePreFab, pos, transform.rotation);
-        
-
-       
-        //if ((gameObject.name == "PlayerOne" && isHit == false))
-        //{
-        //    transform.position += (Vector3)(direction);
-        //    Instantiate(PlayerOnePreFab, pos, transform.rotation);
-        //}
-
-        //else if((gameObject.name == "PlayerTwo" && isHit == false)){
-        //    transform.position += (Vector3)(direction);
-        //    Instantiate(PlayerOnePreFab, pos, transform.rotation);
-        //}
     }
 
-    //void Stop(){
-
-    //    if (gameObject.name == "PlayerOne")
-    //    {
-    //       // Debug.Log("hiii");
-    //        transform.position += (Vector3)(direction * 0);
-    //    }
-
-    //    if ((gameObject.name == "PlayerTwo"))
-    //    {
-    //        transform.position += (Vector3)(direction * 0);
-           
-    //    }
-    //}
 
     void OnTriggerEnter2D(Collider2D other)
     {
        // Debug.Log(other.gameObject.tag);
        // Debug.Log(gameObject.tag);
         isHit = true;
-        SetPointsText();
+
         //speed = 0;
-
-        if(other.gameObject.tag.Equals("Tail")){
-            Debug.Log("I HIT THE TAIL");
-        }
-        //Debug.Log("Is it hit? : " + isHit + " " + gameObject.name);
-        //speed = 0;
-        //if (other.gameObject.tag.Equals("Wall")){
-        //    //speed = 0;
-        //    speedOne = 0;
-        //    speedTwo = 0;
-        //    if (gameObject.tag.Equals("PlayerOne")){
-        //        //Debug.Log("HITTTTT THE WALLL");
-        //        //transform.Translate(direction * speed);
-        //        //speedOne = 0;
-        //        //speedTwo = 0;
-        //        playerTwoScoreCount++;
-        //        SetPointsText();
-        //    }
-
-        //    if(gameObject.tag.Equals("PlayerTwo"))
-        //    {
-        //        //Debug.Log("HITTTTT THE WALLL");
-        //        // transform.Translate(direction * speed);
-        //        //speedOne = 0;
-        //        //speedTwo = 0;
-        //        playerOneScoreCount++;
-        //        SetPointsText();
-
-        //    }
-           
-        //}
-        ////else if (other.gameObject.tag.Equals("Wall") && )
-        ////{
-           
-        ////}
-        //else if (other.gameObject.tag.Equals("Tail"))
-        //{
-        //    if(gameObject.tag.Equals("PlayerOne")){
-        //        //Debug.Log("I hit player one");
-        //        //transform.Translate(direction * speed);
-        //        speedOne = 0;
-        //        speedTwo = 0;
-        //        playerTwoScoreCount++;
-        //        SetPointsText();
-        //    }
-
-        //    else if(gameObject.tag.Equals("PlayerTwo")){
-        //        // Debug.Log("I hit player two");
-        //        //transform.Translate(direction * speed);
-        //        speedOne = 0;
-        //        speedTwo = 0;
-        //        playerOneScoreCount++;
-        //        SetPointsText();
-        //    }
-           
-        //}
-
-        //else if(other.gameObject.tag.Equals("PlayerOne")){
-        //    // Debug.Log("I hit player one");
-        //    //transform.Translate(direction * speed);
-        //    speedOne = 0;
-        //    speedTwo = 0;
-        //    playerTwoScoreCount++;
-        //    SetPointsText();
-        //}
-
-        //else if (other.gameObject.tag.Equals("PlayerTwo"))
-        //{
-        //    // Debug.Log("I hit player one");
-        //    //transform.Translate(direction * speed);
-        //    speedOne = 0;
-        //    speedTwo = 0;
-        //    playerOneScoreCount++;
-        //    SetPointsText();
-        //}
-
-
 
     }
 
-    void SetPointsText()
-    {
-        if (gameObject.tag.Equals("PlayerOne"))
-        {
-            playerTwoScoreCount++;
-            PlayerTwoScore.text = "Player Two: " + playerTwoScoreCount.ToString();
-        }
-        else if (gameObject.tag.Equals("PlayerTwo")) {
-            playerOneScoreCount++;
-            PlayerOneScore.text = "Player One: " + playerOneScoreCount.ToString();
-        }
-        //Debug.Log(GameObject.name);
-        if(gameObject.tag.Equals("PlayerOne")){
-            //transform.position = new Vector3(-1.295f, 0.775001f, 0f);
-        }
-        if (gameObject.tag.Equals("PlayerTwo")){
-          //  transform.position = new Vector3(-1.035015f, 0.755015f, 0f);
-        }
-       
-                
-    }
+   
 
 }
