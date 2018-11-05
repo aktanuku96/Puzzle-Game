b6a5e6a04ac889c6254c08f4c391a63b99eae6df
Ashwin <aktanuku96@gmail.com>
2018-11-05 05:52:02 -0500

    the other stuff left behind

---
+++ Blockade/Assets/Scripts/DisplayText.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayText : MonoBehaviour {
+
+    public Text Stern;
+    public AudioClip hiss;
+    public AudioSource source;
+	// Use this for initialization
+	void Start () {
+        source.clip = hiss;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Stern.enabled = true;
+        source.PlayOneShot(hiss);
+        if (Input.GetKey(KeyCode.R))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("LevelOne");
+        }
+
+    }
+}
